f72b7cef41a684ea095b50b47be4f025ca45d733
Jellyjones-pixel <johnmbithimutave@gmail.com>
2023-06-22 10:57:24 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -15,19 +15,3 @@ void print_numbers(void)
 	}
 	_putchar('\n');
 }
-
-
-3-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-    print_numbers();
-    return (0);
-}
